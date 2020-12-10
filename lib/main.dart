e8dd526c8df0b4084356ceeb58d0680c50a46e61
blu_sr <blu_sr@yahoo.com>
2020-12-10 11:06:16 +0600

    Merge branch 'master' of https://github.com/rahmansunbeam/test_6

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      showPerformanceOverlay: true,
       initialRoute: '/',
       routes: {
         '/': (context) => SplashScreen(),
--- lib/services/words_service.dart
+++ lib/services/words_service.dart
@@ -31,113 +31,131 @@ class _WordsToRenderState extends State<WordsToRender> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Container(
-            constraints: BoxConstraints(
-                maxHeight: double.infinity, minHeight: _height / 100 * 15),
-            child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    listOfWordsToMakeChips(
-                        widget.listSelected
-                            ? widget.listOfWordset[0][widget.index]['ANTONYMS']
-                                .split('| ')
-                            : widget.data[widget.index]['ANTONYMS'].split('| '),
-                        Colors.amber),
-                    listOfWordsToMakeChips(
-                        widget.listSelected
-                            ? widget.listOfWordset[0][widget.index]['SYNONYMS']
-                                .split('| ')
-                            : widget.data[widget.index]['SYNONYMS'].split('| '),
-                        Colors.tealAccent),
-                  ],
-                )),
-          ),
-          Container(
-            constraints: BoxConstraints(
-                maxHeight: _height / 100 * 7, minHeight: _height / 100 * 7),
-            alignment: Alignment.bottomLeft,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Text(
-                widget.listSelected
-                    ? widget.listOfWordset[0][widget.index]['MEANINGS']
-                    : widget.data[widget.index]['MEANINGS'],
-                style: TextStyle(
-                    color: widget.darkThemeChosen ? Colors.white : Colors.black,
-                    fontSize: (_width < 350)
-                        ? 11.0 * MediaQuery.textScaleFactorOf(context)
-                        : (_width >= 350 && _width < 600)
-                            ? 12.0 * MediaQuery.textScaleFactorOf(context)
-                            : 16.0 * MediaQuery.textScaleFactorOf(context)),
-              ),
-            ),
-          ),
-          Container(
-            constraints: BoxConstraints(
-                maxHeight: double.infinity, minHeight: _height / 100 * 5),
-            alignment: Alignment.centerLeft,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Text(
-                widget.listSelected
-                    ? widget.listOfWordset[0][widget.index]['WORDS']
-                    : widget.data[widget.index]['WORDS'],
-                style: TextStyle(
-                    color: widget.darkThemeChosen
-                        ? Colors.white
-                        : Colors.grey[700],
-                    fontFamily: 'Roboto Slab',
-                    fontSize: (_width < 350)
-                        ? 22.0 * MediaQuery.textScaleFactorOf(context)
-                        : (_width >= 350 && _width < 600)
-                            ? 25.0 * MediaQuery.textScaleFactorOf(context)
-                            : 30.0 * MediaQuery.textScaleFactorOf(context)),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-            child: Container(
-              height: 3.5,
-              alignment: Alignment.center,
-              child: LinearProgressIndicator(
-                value: (widget.listSelected
-                    ? widget.listOfWordset[0]
-                            .indexOf(widget.listOfWordset[0][widget.index]) /
-                        100 *
-                        3.5
-                    : widget.data.indexOf(widget.data[widget.index]) /
-                        100 *
-                        0.12),
-              ),
-            ),
-          ),
+          _synonymsAndAntonyms(_height, _width),
+          _wordMeanings(_height, _width),
+          _words(_height, _width),
+          _progressBar()
         ],
       ),
     );
   }
 
   // Antonyms and synonyms chips are processed
-  Widget listOfWordsToMakeChips(List<String> _item, Color _bgColor) {
+  Widget _listOfWordsToMakeChips(List<String> _item, Color _bgColor) {
     double _width = MediaQuery.of(context).size.width;
-    List<Widget> list = List<Widget>();
+    List<Widget> list = [];
     for (var i = 0; i < _item.length; i++) {
       if (_item[i] != 'N/A' && _item[i] != '') {
-        list.add(Chip(
-            label: Text(_item[i],
+        list.add(Padding(
+          padding: const EdgeInsets.only(top: 5, bottom: 5),
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+                color: _bgColor, borderRadius: BorderRadius.all(Radius.circular(20))),
+            child: Text(_item[i],
                 style: TextStyle(
-                  fontSize: (_width < 350)
+                  fontSize: (_width < 375)
                       ? 11.0 * MediaQuery.textScaleFactorOf(context)
-                      : (_width >= 350 && _width < 600)
+                      : (_width >= 375 && _width < 600)
                           ? 12.0 * MediaQuery.textScaleFactorOf(context)
                           : 14.0 * MediaQuery.textScaleFactorOf(context),
                 )),
-            backgroundColor: _bgColor));
+          ),
+        ));
       }
     }
     return Wrap(runSpacing: -16, spacing: 2, children: list);
+  }
+
+  Widget _synonymsAndAntonyms(double _height, double _width) {
+    return Container(
+      constraints:
+          BoxConstraints(maxHeight: double.infinity, minHeight: _height / 100 * 15),
+      child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              _listOfWordsToMakeChips(
+                  widget.listSelected
+                      ? widget.listOfWordset[0][widget.index]['ANTONYMS'].split('| ')
+                      : widget.data[widget.index]['ANTONYMS'].split('| '),
+                  Colors.amber),
+              _listOfWordsToMakeChips(
+                  widget.listSelected
+                      ? widget.listOfWordset[0][widget.index]['SYNONYMS'].split('| ')
+                      : widget.data[widget.index]['SYNONYMS'].split('| '),
+                  Colors.tealAccent),
+            ],
+          )),
+    );
+  }
+
+  Widget _wordMeanings(double _height, double _width) {
+    return Container(
+      alignment: Alignment.bottomLeft,
+      constraints:
+          BoxConstraints(maxHeight: _height / 100 * 7, minHeight: _height / 100 * 6),
+      child: Scrollbar(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Text(
+            widget.listSelected
+                ? widget.listOfWordset[0][widget.index]['MEANINGS']
+                : widget.data[widget.index]['MEANINGS'],
+            style: TextStyle(
+                color: widget.darkThemeChosen ? Colors.white : Colors.black,
+                fontSize: (_width < 375)
+                    ? 13.0 * MediaQuery.textScaleFactorOf(context)
+                    : (_width >= 375 && _width < 600)
+                        ? 14.0 * MediaQuery.textScaleFactorOf(context)
+                        : 16.0 * MediaQuery.textScaleFactorOf(context)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _words(double _height, double _width) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        padding: const EdgeInsets.only(top: 4.0),
+        alignment: Alignment.centerLeft,
+        constraints:
+            BoxConstraints(maxHeight: double.infinity, minHeight: _height / 100 * 5),
+        child: Text(
+          widget.listSelected
+              ? widget.listOfWordset[0][widget.index]['WORDS']
+              : widget.data[widget.index]['WORDS'],
+          style: TextStyle(
+              color: widget.darkThemeChosen ? Colors.white : Colors.grey[700],
+              fontFamily: 'Roboto Slab',
+              fontSize: (_width < 375)
+                  ? 20.0 * MediaQuery.textScaleFactorOf(context)
+                  : (_width >= 375 && _width < 600)
+                      ? 22.0 * MediaQuery.textScaleFactorOf(context)
+                      : 30.0 * MediaQuery.textScaleFactorOf(context)),
+        ),
+      ),
+    );
+  }
+
+  Widget _progressBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+      child: Container(
+        height: 3.5,
+        alignment: Alignment.center,
+        child: LinearProgressIndicator(
+          value: (widget.listSelected
+              ? widget.listOfWordset[0].indexOf(widget.listOfWordset[0][widget.index]) /
+                  100 *
+                  3.5
+              : widget.data.indexOf(widget.data[widget.index]) / 100 * 0.12),
+        ),
+      ),
+    );
   }
 }
